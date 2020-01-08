40a42892a637b52b7086f500831b9da09b1f7db5
JohnDoeMask <heyuehuang@hotmail.com>
2020-01-08 18:00:43 -0500

    Print source code

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -4,5 +4,10 @@ import requests
 # print(requests.__version__)
 
 # curl
-response = requests.get('http://google.com')
-print(response)
+# response = requests.get('http://google.com')
+# print(response)
+
+# Print source code
+rawURL = 'https://raw.githubusercontent.com/JohnDoeMask/CMPUT404Labs/master/Lab1/Lab1.py'
+response = requests.get(rawURL)
+print(response.text)
